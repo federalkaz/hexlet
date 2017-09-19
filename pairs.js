3ca412e08df6fd41de11bbb13dbdbad72e1fc464
federalkaz <svechnikov-d@mail.ru>
2017-09-19 16:24:29 +0600

    JS course, pair. Update.

---
+++ pairs.js
@@ -0,0 +1,14 @@
+function reversePair(pair)
+{
+    return makePair(getY(pair), getX(pair));
+}
+
+function sumOfPairs(pair1, pair2)
+{
+    return makePair(getX(pair1) + getX(pair2), getY(pair1) + getY(pair2));
+}
+
+function ()
+{
+    return ;
+}
